abcc93d6c9c512b2d558afe093d6191d1acaeeec
HlebYakhnitski <161834651+HlebYakhnitski@users.noreply.github.com>
2024-04-11 23:31:48 +0200

    Delete WebApplication1 directory

--- WebApplication1/DataStore.cs
+++
@@ -1,7 +0,0 @@
-namespace WebApplication1;
-
-public static class DataStore
-{
-    public static List<Animal> Animals = new List<Animal>();
-    public static List<Visit> Visits = new List<Visit>();
-}
