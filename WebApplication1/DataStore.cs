6448b38e44b80504ab5fad5cc989fd797cbd845f
s26879 <s26879pjwstk.edu.pl>
2024-04-11 23:36:41 +0200

    Apbd 5

---
+++ WebApplication1/DataStore.cs
@@ -0,0 +1,7 @@
+namespace WebApplication1;
+
+public static class DataStore
+{
+    public static List<Animal> Animals = new List<Animal>();
+    public static List<Visit> Visits = new List<Visit>();
+ }
